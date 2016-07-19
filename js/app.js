f86bfc8031a14874cb21c37b5cd8972eebdbe4ae
Lizza Reyes <lizzareyes13@gmail.com>
2016-07-18 21:14:31 -0400

    arcade game update

--- js/app.js
+++ js/app.js
@@ -1,126 +1,115 @@
 // Enemies our player must avoid
-var Enemy = function(x,y,speed){
-    // Variables applied to each of our instances go here,
+class Enemy {
+  constructor(x,y,speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
-    // we've provided one for you to get started
+    this.sprite = 'images/enemy-bug.png'
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    this.x = this.x+this.speed*dt;
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-
-  if (this.speed < 0){
-    ctx.save();
-    ctx.translate(this.x + 50,0);
-    ctx.scale(-1,1);
-    ctx.translate(-this.x - 50, 0);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    ctx.restore();
+  }
+  render () {
+    if (this.speed < 0){
+      ctx.save();
+      ctx.translate(this.x + 50,0);
+      ctx.scale(-1,1);
+      ctx.translate(-this.x - 50, 0);
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+      ctx.restore();
 
 
 
+      }
+      else{
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+      }
+
+  }
+
+  update (dt) {
+    this.x += this.speed *dt;
+    if (this.x < 0){
+      this.x = 500;
     }
-    else{
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+    if (this.x > 500){
+      this.x = 0;
     }
-
-
-
-
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-function Player(x,y){
-  this.x = x;
-  this.y = y;
-
-  this.sprite = 'images/char-cat-girl.png';
-
-}
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
-};
-Player.prototype.update = function() {
-  if (this.x < 0){
-    this.x = 0;
-  }
-  if (this.x >= 400){
-    this.x = 400;
-  }
-  if (this.y < 50){
-    this.x = 100;
-    this.y = 300;
-  }
-  if (this.y > 400){
-    this.y = 400;
-  }
-  if (this.checkCollisions(allEnemies[0])){
-    this.x = 200;
-    this.y = 400;
-    
-  }
-};
-
-Player.prototype.handleInput = function(dir) {
-
-  if (dir === "left"){
-    this.x = this.x - 100;
-  }
-  if (dir === "right"){
-    this.x = this.x + 100;
-  }
-  if (dir === "up"){
-    this.y = this.y - 50;
-  }
-  if (dir === "down"){
-    this.y = this.y + 50;
   }
 
 }
-Player.prototype.checkCollisions = function(enemy) {
-  var a = this.x;
-  var b = this.y;
-  var c = this.x + 101;
-  var d = this.y;
-  var e = this.x;
-  var f = this.y + 171;
-  var g = this.x + 101;
-  var h = this.y + 171;
-  var a0 = enemy.x;
-  var b0 = enemy.y;
-  var a1 = enemy.x + 101;
-  var b2 = enemy.y + 171;
+class Player {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-cat-girl.png';
+  }
+  render () {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
-  if (a>a0 && a<a1 && b>b0 && b<b2)
-    return true;
-  else if (c>a0 && c<a1 && d>b0 && d<b2)
-    return true;
-  else if (g>a0 && g<a1 && h>b0 && h<b2)
-    return true;
-  else if (e>a0 && e<a && f>b0 && f<b2)
-    return true;
+  }
+  update () {
+    if (this.x < 0){
+      this.x = 0;
+    }
+    if (this.x >= 400){
+      this.x = 400;
+    }
+    if (this.y < 50){
+      this.x = 100;
+      this.y = 300;
+    }
+    if (this.y > 400){
+      this.y = 400;
+    }
+    if (this.checkCollisions(allEnemies[0])){
+      this.x = 200;
+      this.y = 400;
+
+    }
+  }
+
+  handleInput (dir) {
+
+    if (dir === "left"){
+      this.x = this.x - 100;
+    }
+    if (dir === "right"){
+      this.x = this.x + 100;
+    }
+    if (dir === "up"){
+      this.y = this.y - 50;
+    }
+    if (dir === "down"){
+      this.y = this.y + 50;
+    }
+
+  }
+  checkCollisions (enemy) {
+    var a = this.x;
+    var b = this.y;
+    var c = this.x + 101;
+    var d = this.y;
+    var e = this.x;
+    var f = this.y + 171;
+    var g = this.x + 101;
+    var h = this.y + 171;
+    var a0 = enemy.x;
+    var b0 = enemy.y;
+    var a1 = enemy.x + 101;
+    var b2 = enemy.y + 171;
+
+    if (a>a0 && a<a1 && b>b0 && b<b2)
+      return true;
+    else if (c>a0 && c<a1 && d>b0 && d<b2)
+      return true;
+    else if (g>a0 && g<a1 && h>b0 && h<b2)
+      return true;
+    else if (e>a0 && e<a && f>b0 && f<b2)
+      return true;
 
 
-  return false;
-};
+    return false;
+  }
+}
 
 // Now instantiate your objects.
 var player = new Player(0,0);
